ed416f2b494a3e7cfef664e256be63900096f592
Kyle Isom <coder@kyleisom.net>
2011-05-11 00:37:36 -0600

    make url_tools standalone and more robust
    add error handling: checks for HTML parser errors and HTTP errors
    
    now has a standalone function: will load the url specified on the CLI and
    display the title

--- url_tools.py
+++ url_tools.py
@@ -1,18 +1,36 @@
+#!/usr/bin/env python
 # grab a site's title
+# TODO: add error checking to display()
 
+import BeautifulSoup
+import sys
 import urllib2
-from BeautifulSoup import BeautifulSoup
 
 def load(url):
-    soup = BeautifulSoup(urllib2.urlopen(url).read())
-
-    title = soup.find('title').contents[0]
+    try:
+        soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(url).read())
+        title = str(soup.find('title').contents[0])
+    except BeautifulSoup.HTMLParseError, e:
+        print 'HTML parser error:', str(e)
+        title = None
+    except urllib2.HTTPError, e:
+        title = e.getcode()
 
     return title
 
 def display(url):
     title = load(url)
-    msg   = '%s: %s' % (url, title)
+
+    if type(title) == type(str()):  
+        msg = '%s: %s' % (url, title)
+    elif type(title) == type(int()):  
+        msg = '%s returned HTTP error %d' % (url, title)
+    else:     
+        msg   = 'failed to load title for %s' % url
 
     return msg
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print display(sys.argv[1])
 
