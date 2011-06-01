de04949e52a7fed67870862cc05f3aa29a041245
Kyle Isom <coder@kyleisom.net>
2011-05-31 22:50:37 -0600

    adding getopt import

--- mailer.py
+++ mailer.py
@@ -56,6 +56,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.audio import MIMEAudio
+import getopt
 import smtplib
 import sys
 
